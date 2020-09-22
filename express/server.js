e59a6b8eded712b9b42578ef11095318d03e1f1c
H-b-IO-T-O-H <importantv@yandex.ru>
2020-09-22 18:23:34 +0300

    asd

--- express/server.js
+++ express/server.js
@@ -34,14 +34,15 @@ const express = http.createServer((req, res) => {
   });
 });
 */
-/*
+
 const fs = require('fs');
 const express = require('express');
 const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
-const bodyParser = require('body-parser');
 
+
+/*
 const router = express.Router();
 router.get('/', (req, res) => {
     res
@@ -52,52 +53,10 @@ router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.get('/index', (req, res) => {
     res.sendFile(root + '/');
-});
+});*/
 
-app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../public/main_page.html')));
-
 module.exports = app;
 module.exports.handler = serverless(app);
-*/
 
-const fs = require('fs');
-const express = require('express');
-const path = require('path');
-const http = require('http')
-const app = express();
-
-const server = http.createServer((req, res) => {
-    console.log('requested', req.url);
-    let pat = path.join(__dirname, '../public')
-    switch (req.url) {
-        case '/' || '/StudHunt':
-            pat += '/main_page.html';
-            break;
-        case '/employers':
-            pat += '/employers_list.html';
-            break;
-        case '/applicants':
-            pat += '/candidates_list.html';
-            break;
-        case '/create_resume':
-            pat += '/candidates.html';
-            break;
-        default:
-            pat += req.url;
-    }
-    fs.readFile(pat, (err, file) => {
-        if (err) {
-            console.log('file read error', path, err);
-            res.write('error in template.html');
-            res.end();
-            return;
-        }
-        res.write(file);
-        res.end();
-    });
-});
-
-module.exports = server;
 
