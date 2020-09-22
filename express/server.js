d49ecb8123e6e02331c30430f39f00cc805a7e10
H-b-IO-T-O-H <importantv@yandex.ru>
2020-09-22 18:16:59 +0300

    asd

--- express/server.js
+++ express/server.js
@@ -1,4 +1,5 @@
 'use strict';
+
 /*
 const fs = require('fs');
 const http = require('http');
@@ -33,6 +34,33 @@ const express = http.createServer((req, res) => {
   });
 });
 */
+/*
+const fs = require('fs');
+const express = require('express');
+const path = require('path');
+const serverless = require('serverless-http');
+const app = express();
+const bodyParser = require('body-parser');
+
+const router = express.Router();
+router.get('/', (req, res) => {
+    res
+
+});
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.post('/', (req, res) => res.json({ postBody: req.body }));
+
+app.get('/index', (req, res) => {
+    res.sendFile(root + '/');
+});
+
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../public/main_page.html')));
+
+module.exports = app;
+module.exports.handler = serverless(app);
+*/
 
 const fs = require('fs');
 const express = require('express');
@@ -42,24 +70,24 @@ const app = express();
 
 const server = http.createServer((req, res) => {
     console.log('requested', req.url);
-    let path = 'public';
+    let pat = path.join(__dirname, '../public')
     switch (req.url) {
         case '/' || '/StudHunt':
-            path += '/main_page.html';
+            pat += '/main_page.html';
             break;
         case '/employers':
-            path += '/employers_list.html';
+            pat += '/employers_list.html';
             break;
         case '/applicants':
-            path += '/candidates_list.html';
+            pat += '/candidates_list.html';
             break;
         case '/create_resume':
-            path += '/candidates.html';
+            pat += '/candidates.html';
             break;
         default:
-            path += req.url;
+            pat += req.url;
     }
-    fs.readFile(path, (err, file) => {
+    fs.readFile(pat, (err, file) => {
         if (err) {
             console.log('file read error', path, err);
             res.write('error in template.html');
