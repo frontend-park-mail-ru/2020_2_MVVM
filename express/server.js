3943667f08d0286f6b86c1912e2d90c2cc088aaa
H-b-IO-T-O-H <importantv@yandex.ru>
2020-09-22 18:54:53 +0300

    asd

--- express/server.js
+++ express/server.js
@@ -45,26 +45,16 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 router.get('/', (req, res) => {
     const root = path.resolve(__dirname, '../public');
-    console.log(root)
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    fs.readFile(root + '/main_page.html', (err, file) => {
-        if (err) {
-            console.log('file read error', path, err);
-            res.write('error in template.html');
-            res.end();
-            return;
-        }
-        res.write(file);
-        res.end();
-    });
+    res.write(root)
+    res.write('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
+    res.end();
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/main_page.html'))});
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../public/main_page.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
