1e0074b1b486e869e25f4d80785c54b1f8991cac
H-b-IO-T-O-H <importantv@yandex.ru>
2020-09-22 18:31:51 +0300

    asd

--- express/server.js
+++ express/server.js
@@ -40,23 +40,38 @@ const express = require('express');
 const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
+const bodyParser = require('body-parser');
 
-
-/*
 const router = express.Router();
 router.get('/', (req, res) => {
-    res
-
+    let pat = path.join(__dirname, '../public/main_page.html')
+    fs.readFile(pat, (err, file) => {
+        if (err) {
+            console.log('file read error', path, err);
+            res.write('error in template.html');
+            res.end();
+            return;
+        }
+        res.write(file);
+        res.end();
+    });
 });
+
+router.get('/', (req, res) => {
+    res.write('<h1>Hello from Express.js!</h1>');
+    res.end();
+});
+
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.get('/index', (req, res) => {
     res.sendFile(root + '/');
-});*/
+});
 
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../public/main_page.html')));
+
 module.exports = app;
 module.exports.handler = serverless(app);
-
-
