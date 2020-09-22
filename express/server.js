202c5f0f27b4b0291cae6b13c7586ecba8494590
H-b-IO-T-O-H <importantv@yandex.ru>
2020-09-22 18:11:06 +0300

    test

--- express/server.js
+++ express/server.js
@@ -1,5 +1,4 @@
 'use strict';
-
 /*
 const fs = require('fs');
 const http = require('http');
@@ -38,15 +37,31 @@ const express = http.createServer((req, res) => {
 const fs = require('fs');
 const express = require('express');
 const path = require('path');
-const serverless = require('serverless-http');
+const http = require('http')
 const app = express();
-const bodyParser = require('body-parser');
 
-const router = express.Router();
-router.get('/', (req, res) => {
-    let pat = path.join(__dirname, '../public/main_page.html')
-    fs.readFile(pat, (err, file) => {
+const server = http.createServer((req, res) => {
+    console.log('requested', req.url);
+    let path = 'public';
+    switch (req.url) {
+        case '/' || '/StudHunt':
+            path += '/main_page.html';
+            break;
+        case '/employers':
+            path += '/employers_list.html';
+            break;
+        case '/applicants':
+            path += '/candidates_list.html';
+            break;
+        case '/create_resume':
+            path += '/candidates.html';
+            break;
+        default:
+            path += req.url;
+    }
+    fs.readFile(path, (err, file) => {
         if (err) {
+            console.log('file read error', path, err);
             res.write('error in template.html');
             res.end();
             return;
@@ -55,12 +70,6 @@ router.get('/', (req, res) => {
         res.end();
     });
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
 
-app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../public/main_page.html')));
+module.exports = server;
 
-module.exports = app;
-module.exports.handler = serverless(app);
