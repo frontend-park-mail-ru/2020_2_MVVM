e010fbe16ae0da25e852572c682851ede09f425d
H-b-IO-T-O-H <importantv@yandex.ru>
2020-09-22 19:00:26 +0300

    last try

--- express/server.js
+++ express/server.js
@@ -44,10 +44,16 @@ const bodyParser = require('body-parser');
 
 const router = express.Router();
 router.get('/', (req, res) => {
-    const root = path.resolve(__dirname, '../public');
-    res.write(root)
-    res.write('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
-    res.end();
+    fs.readFile('public/main_page.html', (err, file) => {
+        if (err) {
+            console.log('file read error', path, err);
+            res.write('error in template.html');
+            res.end();
+            return;
+        }
+        res.write(file);
+        res.end();
+    });
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
