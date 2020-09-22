bb65e4e4c1b6ecef9b21d5b438865a4903930f16
H-b-IO-T-O-H <importantv@yandex.ru>
2020-09-22 18:35:45 +0300

    asd

--- express/server.js
+++ express/server.js
@@ -44,7 +44,7 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 router.get('/', (req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<h1>Hello from Express.js!</h1>');
+    res.sendFile(path.join(__dirname, '../public/main_page.html'))
     res.end();
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
