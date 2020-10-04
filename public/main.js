78445634c3b5622f375d04a540ec565a43cb5d48
Margot <margot07008>
2020-10-04 15:42:14 +0300

    check

--- public/main.js
+++ public/main.js
@@ -3,10 +3,6 @@ import {renderEmployersList} from './js/pages/employersList/employersList.js'
 import {renderPersNavBar} from './js/pages/profile/profile.js'
 import {renderResume} from './js/pages/resume/resume.js'
 import {renderVacancy} from './js/pages/vacancy/vacancy.js'
-//import {renderCandList} from './js/pages/candidatesList/candidatesList.js'
-//import {renderEmployersList} from './js/pages/employersList/employersList.js'
-//import {renderPersNavBar} from './js/pages/profile/profile.js'
-//import {renderEmplList} from './js/pages/resume/resume.js'
 import {renderCandidateSummaryCreation} from "./js/pages/createCandidateSum/createCandidateSum.js";
 //import {renderEmployerSummaryCreation} from "./js/pages/createEmployerSum/createEmployerSum.js";
 import {renderMainPage} from "./js/pages/mainPage/mainPage.js"
@@ -14,14 +10,14 @@ import {renderMainPage} from "./js/pages/mainPage/mainPage.js"
 const app = window.document.getElementById('app');
 
 function start(user) {
-    // renderCandList(user);
+    renderCandList(user);
     // renderEmployersList(user);
     // renderPersNavBar(user, false);
     //  renderCandidateSummaryCreation(user);
     // renderEmployerSummaryCreation(user);
     // renderVacancy(user);
     // renderResume(user);
-    renderMainPage(user);
+    // renderMainPage(user);
 }
 
 start(false);
