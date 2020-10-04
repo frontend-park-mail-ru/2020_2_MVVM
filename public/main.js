cc6f7f90362b3a4b5e8cf91dd20a4d60698c785a
Viktoria Talmaza <vp.talmaza@gmail.com>
2020-10-04 15:26:13 +0300

    style form

--- public/main.js
+++ public/main.js
@@ -2,7 +2,7 @@ import {renderCandList} from './js/pages/candidatesList/candidatesList.js'
 import {renderEmployersList} from './js/pages/employersList/employersList.js'
 import {renderPersNavBar} from './js/pages/profile/profile.js'
 import {renderResume} from './js/pages/resume/resume.js'
-import {renderCandidateSummaryCreation} from "./js/pages/createCandidateSum/createCandidateSum.js";
+// import {renderCandidateSummCaryCreation} from "./js/pages/createCandidateSum/createCandidateSum.js";
 import {renderEmployerSummaryCreation} from "./js/pages/createEmployerSum/createEmployerSum.js";
 import {renderVacancy} from "./js/pages/vacancy/vacancy.js";
 
