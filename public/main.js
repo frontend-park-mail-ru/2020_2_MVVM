c4473dc6a8e335a569d53b96c8cddc8a969f6892
Margot <margot07008>
2020-10-05 09:55:11 +0300

    qwerty

--- public/main.js
+++ public/main.js
@@ -4,7 +4,7 @@ import {renderPersNavBar} from './js/pages/profile/profile.js'
 import {renderResume} from './js/pages/resume/resume.js'
 import {renderVacancy} from './js/pages/vacancy/vacancy.js'
 import {renderCandidateSummaryCreation} from "./js/pages/createCandidateSum/createCandidateSum.js";
-//import {renderEmployerSummaryCreation} from "./js/pages/createEmployerSum/createEmployerSum.js";
+import {renderEmployerSummaryCreation} from "./js/pages/createEmployerSum/createEmployerSum.js";
 import {renderMainPage} from "./js/pages/mainPage/mainPage.js"
 
 const app = window.document.getElementById('app');
