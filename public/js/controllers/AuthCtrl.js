a684a6045453b23eeeb0d1fd5fcbc6b958e40a15
Margot <margot07008>
2020-10-07 14:06:49 +0300

    upd2

--- public/js/controllers/AuthCtrl.js
+++ public/js/controllers/AuthCtrl.js
@@ -8,13 +8,12 @@ export default class AuthCtrl {
             event.preventDefault();
 
             let formData = new FormData();
-            console.log(form);
             formData.append("email", form[0][0].value)
             formData.append("nickname", form[0][1].value)
             formData.append("password", form[0][2].value)
 
             const response = await fetch(
-                "http://95.163.212.36/api/v1/auth/login",
+                "api/v1/auth/login",
                 {
                     body: formData,
                     method: "post",
@@ -22,7 +21,7 @@ export default class AuthCtrl {
             )
             const content = await response.json();
             console.assert(response.ok);
-            if (content.code===200) {
+            if (content.user) {
                 this.router.change('\/mainPage');
             }
         }
