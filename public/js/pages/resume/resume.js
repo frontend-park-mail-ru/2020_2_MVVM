ac60077371b0389cd2d728d1164b852b4db8a9f8
Margot <margot07008>
2020-10-23 23:22:18 +0300

    fix fields

--- public/js/pages/resume/resume.js
+++ public/js/pages/resume/resume.js
@@ -1,6 +1,6 @@
 import {NavBarInit} from "../../components/header/navBar.js";
 import {network} from "../../libs/networks.js";
-import {usersByIdURL, resumeByIdURL} from "../../libs/constants.js";
+import {usersByIdURL, resumeByIdURL, gender, educationLevel,experienceLevel, experienceMonth} from "../../libs/constants.js";
 import createElem from "../../libs/createElem.js";
 
 
@@ -27,10 +27,10 @@ const resumeInfo = async (user_id, resume_id) => {
 
     const resp = {
         resume:{
-            id: "dfvdbf-sdfbc-vbf-b",
-            user_id: "dvsdvs-dv-dfv-dsv-d",
+            id: resume.id,
+            user_id: user.id,
             awards: null,
-            career_level: "middle",
+            career_level: "junior",
             description: "о себе",
             education_level: "middle",
             experience_month: 5,
@@ -62,27 +62,37 @@ const resumeInfo = async (user_id, resume_id) => {
             ],
     }
 
+    const dateRegBd = resp.resume.date_create.toString();
+    let dataReg = '';
+    dataReg = dateRegBd.slice(8,10) + '-' + dateRegBd.slice(5,7) + '-' + dateRegBd.slice(0,4);
 
-    return [{
-        mainSkill: ['TODODesigners'],
-        photo: 'img/es1.jpg',
-        name: user.name + " " + user.surname,
-        position: 'TODO UX / UI Designer at Atract Solutions',
-        mail: user.email,
-        dateReg: 'TODO2017',
-        location: 'TODOМосква / Россия'
-    }, {
-        user_id: resume.id,
-        salary_min: nullToString(resume.salary_min),
-        salary_max: nullToString(resume.salary_max),
-        gender: nullToString(resume.gender),
-        level: nullToString(resume.level),
-        experience_month: nullToString(resume.experience_month),
-        interest: "TODOManagement",
-        education: nullToString(resume.education),
-    },
-        {text: nullToString(resume.description)}
-    ]
+
+    return {
+        infoAll : {
+            photo: 'img/es1.jpg',
+            name: user.name + " " + user.surname,
+            position: resp.resume.place,
+            mail: user.email,
+            dateReg: dataReg,
+            location: 'TODOМосква / Россия',
+        },
+        jobOverview : {
+                name: user.name,
+                salary_min: nullToString(resp.resume.salary_min),
+                salary_max: nullToString(resp.resume.salary_max),
+                gender: nullToString(gender[resp.resume.gender]),
+                experience_level: nullToString(experienceLevel[resp.resume.education_level]),
+                experience_month: nullToString(experienceMonth[resp.resume.experience_month]),
+                interest: "TODOManagement",
+                education: nullToString(educationLevel[resp.resume.education_level]),
+        },
+        description : {
+            text: nullToString(resp.resume.description),
+            experience_custom_company: resp.experience_custom_company,
+            skills: resp.resume.skills,
+        }
+
+    }
 }
 
 export default class Resume {
@@ -99,7 +109,7 @@ export default class Resume {
         const candOptions = createElem("div", "cand-option", app.firstElementChild.firstElementChild.firstElementChild)
 
 
-        candOptions.insertAdjacentHTML("afterEnd", window.fest['briefInfo.tmpl'](infoAll[0]));
+        candOptions.insertAdjacentHTML("afterEnd", window.fest['briefInfo.tmpl'](infoAll.infoAll));
 
         const main = createElem("div", "main", app);
 
@@ -110,13 +120,13 @@ export default class Resume {
 
         const contentLeftColumn = createElem("div", "content-left-column", mainContent);
 
-        contentLeftColumn.insertAdjacentHTML("beforeend", window.fest['resume.tmpl'](infoAll[2]));
+        contentLeftColumn.insertAdjacentHTML("beforeend", window.fest['resume.tmpl'](infoAll.description));
 
 
         const contentRightColumn = createElem("div", "content-right-column", mainContent);
 
 
-        contentRightColumn.insertAdjacentHTML("beforeend", window.fest['jobOverview.tmpl'](infoAll[1]));
+        contentRightColumn.insertAdjacentHTML("beforeend", window.fest['jobOverview.tmpl'](infoAll.jobOverview));
 
         contentRightColumn.insertAdjacentHTML("beforeend", window.fest['contactForm.tmpl']());
 
