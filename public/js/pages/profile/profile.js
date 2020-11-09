3438c95ade7f632fb2e1afce2b923f9881f6f345
margot07008 <9311margarita@gmail.com>
2020-11-09 14:18:19 +0300

    fix

--- public/js/pages/profile/profile.js
+++ public/js/pages/profile/profile.js
@@ -55,7 +55,6 @@ export default class Profile {
             await this.loadFavorites().then((data)=>{
                 this.favorites = data;
             });
-            // работодатель может быть привязан ток к одной компании, поэтому для всех вакансий работодателя компания одна
             await this.loadCompany().then((data) => {
                 this.company = data.company;
             });
